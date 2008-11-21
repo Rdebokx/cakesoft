1f751b3de940e5e6bcff0a8c831efa1a85014bb6
leftblank@leftblank.nl <leftblank@leftblank.nl@bc6f4362-acc5-11dd-b9e7-8d5594d729c2>
2008-11-21 13:52:21 +0000

    Demo bestand voor JavaDoc

---
+++ trunk/src/DemoJavaDoc.java
@@ -0,0 +1,20 @@
+/** 
+ * Klasse DemoJavaDoc, laat JavaDoc gedoe zien, dit is een korte beschrijving van wat de klasse kan en doet.
+ * @author Goep 11
+ * @version 1.0
+ */
+
+public class DemoJavaDoc {
+
+	/**
+	 * demoMethode, laat zien dat je hier een beschrijving intikt van de methode.
+	 * Die uitleg kan over meerdere regels enzo, gewoon voor de mensch.
+	 * @param invoer	Bevat de invoer van de methode
+	 * @param nummer	Gewoon een nummer, je kunt meer dan één parameter meegeven!
+	 * @return			Geeft "Blaat!" terug.
+	 */
+	public String demoMethod(String invoer, int nummer)
+	{
+		return "Blaat!";
+	}
+}
