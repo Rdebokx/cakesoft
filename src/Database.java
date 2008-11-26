e4258153372ee5816d924668cd4b76297629947b
pratend.verkeersbord@gmail.com <pratend.verkeersbord@gmail.com@bc6f4362-acc5-11dd-b9e7-8d5594d729c2>
2008-11-26 09:24:17 +0000

    Met de query methoden

--- src/Database.java
+++ src/Database.java
@@ -1,19 +1,14 @@
+import java.sql.*;
+
 /**
  * klasse Database zorgt voor de interactie tussen het programma en de database.
  * Hij maakt verbinding en voert querys uit.
  *@author Groep 11
  *@version 1.0
  */
-
-
-
-import java.sql.*;
-
-
 public class Database {
 	
 	private Connection connection;
-	private Statement statement = connection.createStatement();
 	private String url;
 	private String username;
 	private String password;
@@ -27,7 +22,6 @@ public class Database {
 	 * @param user	bevat de gebruikersnaam
 	 * @param passw	bevat het wachtwoord dat nodig is om toegang tot de database te verkrijgen
 	 */
-	
 	public Database(String urlin, String user, String passw) // de constructor
 	{
 		String driver = "com.mysql.jdbc.Driver";
@@ -48,18 +42,66 @@ public class Database {
 	}
 	
 	/**
-	 * query, selecteert alle kolommen van een de rijen die aa nde voorwaarde voldoen
+	 * Deze methode selecteert alle kolommen van een de rijen die aan de voorwaarde voldoen
 	 * @param tabel			bevat de naam van de tabel waaruit geselecteerd moet worden.
 	 * @param vwkolom		bevat de kolomnaam waarover de voorwaarde gaat
-	 * @param voorwaarde	bevat de waarde die de kolom moet hebben.
+	 * @param voorwaarde	bevat de waarde die de kolom moet hebben, als string.
 	 * @return 				geeft de resultset van de uitgevoerde query terug.
 	 */
-
 	public ResultSet query(String tabel, String vwkolom, String voorwaarde)
 	{
 		String query = "SELECT * FROM " + tabel + " WHERE " + vwkolom + "=" + voorwaarde;
+		ResultSet res;
 		
-		ResultSet res = statement.executeQuery(query);
+		try{
+			Statement statement = connection.createStatement();
+			res = statement.executeQuery(query);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			System.out.println(e.toString());
+		}
+		
+		return res;
 	}
+	
+	/**
+	 * Deze methode voert een query uit, waarmee je alle colommen selecteerd waarbij de opgegeven kolom voldoet aan de eis.
+	 * Tevens worden de resultaten aan de hand van een opgegeven kolom op- of aflopend gerangschikt
+	 * @param tabel			bevat de naam van de tabel
+	 * @param vwkolom		bevat de naam van de kolom waaraan de voorwaarde wordt gesteld
+	 * @param voorwaarde	bevat de waarde die records in vwkolom moeten hebben 
+	 * @param sorteerOp		bevat de naam van de kolom waarop gesorteerd moet worden
+	 * @param oplopend		Als true, worden de resutlaten oplopend gerangschikt, indien false, aflopend
+	 */
+	public ResultSet query(String tabel, String vwkolom, String voorwaarde, String sorteerOp, boolean oplopend)
+	{
+		String query = "SELECT * FROM " + tabel + " WHERE " + vwkolom + "=" + voorwaarde; //dit gedeelte van de statement moet zowiezo worden uitgevoerd	
+		if(oplopend)
+		{	//als oplopend true is, moeten de resultaten oplopend geordend worden aan de hand van sorteerOp
+			query = query + " ORDER BY " + sorteerOp + " ASC";
+		}
+		else if(!oplopend)
+		{ //als oplopend false is, moeten de resultaten aflopend worden geordend aan de hand van sorteerOp 
+			query = query + " ORDER BY " + sorteerOp + " DESC";
+		}
+		
+		ResultSet res;
+		try
+		{
+			Statement statement = connection.createStatement();
+			res = statement.executeQuery(query);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			System.out.println(e.toString());
+		}
+		
+		return res;
+		
+	}
+	
 	
 }
