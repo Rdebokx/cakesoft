e146d8d67637cc21e42103b3a31f4fe8fca51b3a
pratend.verkeersbord@gmail.com <pratend.verkeersbord@gmail.com@bc6f4362-acc5-11dd-b9e7-8d5594d729c2>
2008-11-27 21:21:29 +0000

    De beheerDeelnemer klasse, met alle bijbehorende methoden

---
+++ trunk/src/beheerDeelnemer.java
@@ -0,0 +1,38 @@
+
+/**
+ * Deze methode bevat alle methoden voor het werken met Deelnemer objecten.
+ * @author Groep 11
+ * @version 1.0
+ *
+ */
+public class beheerDeelnemer {
+	
+	Database database;
+	
+	/**
+	 * Deze methode is de constructor en maakt een nieuw beheerDeelnemer object aan, gekoppelt aan de opgegeven database
+	 * @param db	
+	 */
+	public beheerDeelnemer(Database db)
+	{
+		database = db;
+	}
+	
+	/**
+	 * Deze methode voegt een nieuwe deelnemer toe aan de database en set de id van de deelnemer,
+	 * aan de hand van de auto-increment functie van de database
+	 * @param deelnemer		bevat het deelnemer object dat moet worden toegevoegd
+	 * @param baksel		bevat het baksel waaraan de deelnemer gekoppelt is.
+	 * @param wedstrijd		bevat de wedstrijd waaraan de deelnemer meedoet.
+	 */
+	public void voegDeelnemerToe(Deelnemer deelnemer, Baksel baksel, Wedstrijd wedstrijd)
+	{
+		String waarden = "," + deelnemer.getLid_id() + "," + wedstrijd.getWedstrijd_id() + "," + baksel.getBaksel_id();  //eerste waarde niet invoeren, is auto-increment
+		
+		//de gegevens in de database invoeren
+		int deelnemer_id = database.insert("deelnemer", waarden);
+		deelnemer.setDeelnemer_id(deelnemer_id);
+	}
+	
+
+}
